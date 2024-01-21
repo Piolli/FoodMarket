bbe371af0c415eebc9209340793965f72f3a735f
Alexander <pocharua@mail.ru>
2024-01-21 21:23:06 +0700

    Initial commit

---
+++ FoodMarket/FoodMarketApp.swift
@@ -0,0 +1,20 @@
+//
+//  FoodMarketApp.swift
+//  FoodMarket
+//
+//  Created by Alex on 21.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct FoodMarketApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
