0f6f1c397d7744e33c0a3739b98d4f171cedcb8e
Alexander <pocharua@mail.ru>
2024-02-28 01:17:24 +0700

    Add scrolling to selected item in horizontal capsules view

--- FoodMarket/HorizontalCapsulesView.swift
+++ FoodMarket/HorizontalCapsulesView.swift
@@ -16,9 +16,14 @@ class HorizontalCapsuleViewModel: ObservableObject {
         self.items = items
     }
     
-    func getBindingIsOn(for item: HorizontalCapsuleItem) -> Binding<Bool> {
+    func getBindingIsOn(for item: HorizontalCapsuleItem, scrollViewProxy: ScrollViewProxy) -> Binding<Bool> {
         return Binding<Bool>(get: { self.selectedItem == item },
-                             set: { _ in self.selectedItem = item })
+                             set: { _ in
+            self.selectedItem = item
+            withAnimation {
+                scrollViewProxy.scrollTo(item.id, anchor: .center)
+            }
+        })
     }
     
     deinit {
@@ -41,29 +46,33 @@ struct HorizontalCapsulesView: View {
     @ObservedObject var viewModel: HorizontalCapsuleViewModel
     
     var body: some View {
-        ScrollView(.horizontal) {
-            HStack(spacing: 8) {
-                ForEach(viewModel.items) { item in
-                    Toggle(item.name, isOn: viewModel.getBindingIsOn(for: item))
-                        .toggleStyle(ButtonableToggleStyle())
+        ScrollViewReader { scrollPosition in
+            ScrollView(.horizontal) {
+                HStack(spacing: 8) {
+                    ForEach(viewModel.items) { item in
+                        Toggle(item.name, isOn: viewModel.getBindingIsOn(for: item, scrollViewProxy: scrollPosition))
+                            .toggleStyle(ButtonableToggleStyle())
+                            .id(item.id)
+                    }
                 }
+                .padding([.leading, .trailing], 8)
+                .padding([.top, .bottom], 12)
             }
-            .padding([.leading, .trailing], 8)
-            .padding([.top, .bottom], 12)
-        }
-        .background(Rectangle()
-            /// TODO: add some color?
-//            .fill(Color.gray.opacity(0.2))
-            .fill(Material.ultraThin)
-            .clipShape(.rect(bottomLeadingRadius: 16, bottomTrailingRadius: 16))
-        )
+            .background(Rectangle()
+                /// TODO: add some color?
+    //            .fill(Color.gray.opacity(0.2))
+                .fill(Material.ultraThin)
+                .clipShape(.rect(bottomLeadingRadius: 16, bottomTrailingRadius: 16))
+            )
         .scrollIndicators(.never)
+        }
     }
 }
 
 #Preview {
     VStack {
-        HorizontalCapsulesView(viewModel: .init(items: [.init(name: "Name #1", isToggled: false), .init(name: "Name #2", isToggled: true), .init(name: "Name #3", isToggled: false)]))
+        HorizontalCapsulesView(viewModel: .init(items: (0...15).map { .init(name: "Item #\($0)", 
+                                                                            isToggled: false) }))
         Spacer()
     }
 }
